10139e5efd63f8159a4c4fe67483bf50037b3541
Federico Churca-Torrusio <federico.churca@gmail.com>
2013-05-21 19:17:58 -0300

    Arreglo retorno de getIntAnswer, getYesOrNoAnswer

--- LibX.sh
+++ LibX.sh
@@ -1,27 +1,34 @@
 #
 
 # Obtener respuesta entero
+# $1 debe llevar el nombre de la variable de retorno
 function getIntAnswer {
-local ans=
-while [ -z "${ans##*[!0-9]*}" ]; do
-	read ans
-	if [ -z "${ans##*[!0-9]*}" ]; then
+local __answer__=
+local end=false
+while ! $end; do
+	read __answer__
+	if [ -z "${__answer__##*[!0-9]*}" ]; then
 		echo "Por favor ingrese un número entero"
+	else
+		end=true
 	fi
 done
-return $ans
+eval "$1=${__answer__}"
 }
 
 # Obtener respuesta Sí o No
 # $1 debe llevar el nombre de la variable de retorno
 function getYesOrNoAnswer {
 local __answer__=
-while [ \( "$__answer__" != "si" \) -a \( "$__answer__" != "no" \) ]; do
+local end=false
+while ! $end; do
 	read __answer__
 	# tr trabaja de a bytes, y, aunque funciona así, posiblemente sea mejor cambiar eso con un sed: sed 's/[[:upper:]]*/\L&/;s/í/i/g'
 	__answer__=$(tr [:upper:]Íí [:lower:]ii <<< $__answer__)
 	if [ \( "$__answer__" != "si" \) -a \( "$__answer__" != "no" \) ]; then
 		echo "Por favor responda sí o no"
+	else
+		end=true
 	fi
 done
 eval "$1=${__answer__}"
